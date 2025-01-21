64a2fc513b8975c727d66bdeb9607dca6666b656
casht0wn <jesse.j.turner@gmail.com>
2025-01-21 08:36:36 -0500

    second attempt at mobile fix

--- src/scenes/game_scene/utils/stripe_manager.gd
+++ src/scenes/game_scene/utils/stripe_manager.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 @export var stripe_scene: PackedScene
 @export var stripe_spacing: float = 200.0 # Distance between stripes
-@export var base_scroll_speed: float = 800.0 # Base speed of stripe movement
+@export var base_scroll_speed: float = 720.0 # Base speed of stripe movement
 @export var amplitude_reduction_rate: float = 0.96 # Rate at which amplitude reduces
-@export var speed_reduction_rate: float = 0.985 # Rate at which speed reduces
-@export var width_increase_rate: float = 1.01 # Rate at which width increases
+@export var speed_reduction_rate: float = 0.987 # Rate at which speed reduces
+@export var width_increase_rate: float = 1.02 # Rate at which width increases
 
 @onready var audio_bus_index: int = AudioServer.get_bus_index("Master") # Replace "Master" with the name of your audio bus if different
 @onready var spectrum_analyzer: AudioEffectSpectrumAnalyzerInstance = AudioServer.get_bus_effect_instance(audio_bus_index, 0) as AudioEffectSpectrumAnalyzerInstance
@@ -14,7 +14,8 @@ var elapsed_distance: float = 0.0
 var stripes: Array[Node2D] = []
 
 func _ready():
-	base_scroll_speed = get_viewport().size.y * speed_reduction_rate
+	var scroll_offset = (1 - speed_reduction_rate) + 1
+	base_scroll_speed = get_viewport().size.y * scroll_offset
 
 func _process(delta: float) -> void:
 	# Move all stripes downward
@@ -25,7 +26,7 @@ func _process(delta: float) -> void:
 		stripe.line_width *= width_increase_rate		 # Increase the stripe's width
 
 		# Remove stripes that move off-screen
-		if stripe.position.y > get_viewport().size.y + 100 || stripe.amplitude < 0.00001:
+		if stripe.position.y > get_viewport().size.y || stripe.amplitude < 0.0001:
 			stripes.erase(stripe)
 			stripe.queue_free()
 
